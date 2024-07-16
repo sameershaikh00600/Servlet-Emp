6d63488c5096c03b982b857a8d6dd7128f705cf3
sameer shaikh <sameershaikh00600@gmail.com>
2024-07-17 01:17:18 +0530

    firstt

---
+++ src/main/java/net/javag/Dao/package-info.java
@@ -0,0 +1,1 @@
+package net.javag.Dao;
---
+++ src/main/java/net/javag/controller/EmployeeServlet.java
@@ -0,0 +1,54 @@
+package net.javag.controller;
+import net.javag.model.*;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import net.javag.Dao.EmployeeDao;
+
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+
+
+@jakarta.servlet.annotation.WebServlet("/register")
+public class EmployeeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private EmployeeDao employeeDao;
+
+    public void init() {
+        employeeDao = new EmployeeDao();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String address = request.getParameter("address");
+        String contact = request.getParameter("contact");
+
+        Employee employee = new Employee();
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employee.setUsername(username);
+        employee.setPassword(password);
+        employee.setContact(contact);
+        employee.setAddress(address);
+
+        try {
+            employeeDao.registerEmployee(employee);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("employeedetails.jsp");
+    }
+}
